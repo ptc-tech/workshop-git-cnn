08176a2748599e22063e8eb133c7c1e73af330a6
CharlotteBimou1 <50669298+CharlotteBimou1@users.noreply.github.com>
2021-07-08 11:43:31 +0200

    Ajout du modele

--- main_file.py
+++ main_file.py
@@ -38,7 +38,17 @@ from flask import jsonify, make_response, request
 
 #      1. CREATION DU MODELE CNN
 
-
+model = tf.keras.Sequential(
+    [
+     keras.Input(shape=(32, 32, 3)),  
+     layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+     layers.MaxPooling2D(pool_size=(2, 2)),
+     layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+     layers.MaxPooling2D(pool_size=(2, 2)),
+     layers.Flatten(),
+     layers.Dropout(0.5),
+    ]
+)
 
 # ====================================== #
 
