df2445c830d7377c1afbf15f1ec4cbab991bbcb2
Saad Touhbi <stouhbi@gmail.com>
2021-07-24 02:07:18 +0200

    adding routes
    seems there is a problem with tensorflow

--- main_file.py
+++ main_file.py
@@ -25,7 +25,9 @@ from flask import jsonify, make_response, request
 ##############################################################################
 (images_train, labels_train), (images_test, labels_test) = keras.datasets.mnist.load_data()
 
-
+# initlize variables
+model = None
+history = None
 # ====================================== #
 
 #      2. VISUALISATION DES INPUTS
@@ -37,33 +39,38 @@ from flask import jsonify, make_response, request
 # ====================================== #
 
 #      1. CREATION DU MODELE CNN
+def create_model():
+    model = keras.Sequential(
+        [
+         layers.Input(shape=(32, 32, 3)),
+         layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+         layers.MaxPooling2D(pool_size=(2, 2)),
+         layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+         layers.MaxPooling2D(pool_size=(2, 2)),
+         layers.Flatten(),
+         layers.Dropout(0.5),
+         layers.Dense(10, activation='softmax')
+        ]
+    )
 
-model = keras.Sequential(
-    [
-     keras.Input(shape=(32, 32, 3)),
-     layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-     layers.MaxPooling2D(pool_size=(2, 2)),
-     layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-     layers.MaxPooling2D(pool_size=(2, 2)),
-     layers.Flatten(),
-     layers.Dropout(0.5),
-     layers.Dense(10, activation='softmax')
-    ]
-)
+    model.compile(optimizer="Adam", loss="mse", metrics=["acc"])
 
-model.compile(optimizer="Adam", loss="mse", metrics=["acc"])
+    return "<p>model built!</p>"
 
 # ====================================== #
 
 # ====================================== #
 
 #      4. TRAINING
+def train_model():
+    if model is None:
+        return "<p>you should create the model first using the rout /create_model</p>"
+    else:
+        batch_size = parameters["batch_size"]
+        nb_epochs = parameters["epochs"]
+        validation_split = 0.2
 
-batch_size = parameters["batch_size"]
-nb_epochs = parameters["epochs"]
-validation_split = 0.2
-
-history = model.fit(images_train, labels_train, batch_size=batch_size, epochs=nb_epochs, validation_split= validation_split)
+        history = model.fit(images_train, labels_train, batch_size=batch_size, epochs=nb_epochs, validation_split= validation_split)
 
 
 # ====================================== #
@@ -71,7 +78,8 @@ history = model.fit(images_train, labels_train, batch_size=batch_size, epochs=nb
 # ====================================== #
 
 #      2. VISUALISATION DES RESULTATS
-
+def visualize_result():
+    return "<p></p>"
 
 
 # ====================================== #
@@ -80,7 +88,12 @@ history = model.fit(images_train, labels_train, batch_size=batch_size, epochs=nb
 
 #      5. EVALUATION & PRÉDICTION
 
-
+def predict(image):
+    if history is None:
+        return "<p>you should train the model first using the rout /create_model</p>"
+    else:
+        return ""
+# part of the prediction
 
 # ====================================== #
 
@@ -90,6 +103,25 @@ history = model.fit(images_train, labels_train, batch_size=batch_size, epochs=nb
 
 app = Flask(__name__)
 
+@app.route("/create_model")
+def call_create_model():
+    return create_model()
+
+@app.route("/train_model")
+def call_train_model():
+    return train_model()
+
+@app.route("/visualize_result")
+def call_visualize_result():
+    return visualize_result()
+
+@app.route("/predict")
+def call_predict(image):
+    return predict(image)
+
+@app.route('/')
+def index():
+    return 'index'
 
 if __name__ == "__main__":
     app.run()
