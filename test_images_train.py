eb4519f671ff23ed28f91e9ad6b556bb1e58b5e5
Benjamin Cohen-Lhyver <cohenlhyver@protonmail.com>
2021-07-25 09:47:33 -0700

    minor bug fixed

--- test_images_train.py
+++ test_images_train.py
@@ -2,15 +2,16 @@
 # @Author: Benjamin Cohen-Lhyver
 # @Date:   2021-07-24 10:15:59
 # @Last Modified by:   Benjamin Cohen-Lhyver
-# @Last Modified time: 2021-07-25 08:27:24
+# @Last Modified time: 2021-07-25 08:47:57
 
 import pickle
 
-with open("training_history.csv", "rb") as infile:
-    history = pickle.load(infile)
+if __name__ == "__main__":
 
+    with open("training_history.csv", "rb") as infile:
+        history = pickle.load(infile)
 
-if history.accuracy[-1] < 0.95 and history.loss > 0.1:
-    return 0
+    if history.accuracy[-1] < 0.95 and history.loss > 0.1:
+        return 0
 
-return 1
+    return 1
