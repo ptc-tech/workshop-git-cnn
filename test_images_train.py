b6526503e9aaf22eef69e54d733be7be4d398a04
Benjamin Cohen-Lhyver <cohenlhyver@protonmail.com>
2021-07-25 19:17:13 -0700

    switched to assert expressions

--- test_images_train.py
+++ test_images_train.py
@@ -2,16 +2,14 @@
 # @Author: Benjamin Cohen-Lhyver
 # @Date:   2021-07-24 10:15:59
 # @Last Modified by:   Benjamin Cohen-Lhyver
-# @Last Modified time: 2021-07-25 08:47:57
+# @Last Modified time: 2021-07-25 19:16:26
 
 import pickle
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    with open("training_history.csv", "rb") as infile:
-        history = pickle.load(infile)
+with open("training_history.csv", "rb") as infile:
+    history = pickle.load(infile)
 
-    if history.accuracy[-1] < 0.95 and history.loss > 0.1:
-        return 0
-
-    return 1
+assert history["accuracy"][-1] < 0.95
+assert history["loss"] < 0.1
