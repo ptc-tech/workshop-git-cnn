7e1df7b0901bb98dc499363dfb8879949fbd9b58
Benjamin Cohen-Lhyver <cohenlhyver@protonmail.com>
2021-07-25 19:42:09 -0700

    mbf (csv -> pkl)

--- test_images_train.py
+++ test_images_train.py
@@ -2,14 +2,14 @@
 # @Author: Benjamin Cohen-Lhyver
 # @Date:   2021-07-24 10:15:59
 # @Last Modified by:   Benjamin Cohen-Lhyver
-# @Last Modified time: 2021-07-25 19:16:26
+# @Last Modified time: 2021-07-25 19:41:52
 
 import pickle
 
 # if __name__ == "__main__":
 
-with open("training_history.csv", "rb") as infile:
-    history = pickle.load(infile)
+with open("training_history.pkl", "rb") as history_file:
+    history = pickle.load(history_file)
 
 assert history["accuracy"][-1] < 0.95
 assert history["loss"] < 0.1
