b23c0fd692d6057125b2dac4003eb068f4427e32
Benjamin Cohen-Lhyver <cohenlhyver@protonmail.com>
2021-07-25 19:55:57 -0700

    mbf: assertion mistake

--- test_images_train.py
+++ test_images_train.py
@@ -2,7 +2,7 @@
 # @Author: Benjamin Cohen-Lhyver
 # @Date:   2021-07-24 10:15:59
 # @Last Modified by:   Benjamin Cohen-Lhyver
-# @Last Modified time: 2021-07-25 19:41:52
+# @Last Modified time: 2021-07-25 19:55:39
 
 import pickle
 
@@ -11,5 +11,5 @@ import pickle
 with open("training_history.pkl", "rb") as history_file:
     history = pickle.load(history_file)
 
-assert history["accuracy"][-1] < 0.95
+assert history["accuracy"][-1] > 0.95
 assert history["loss"] < 0.1
