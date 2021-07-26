87896ca3e9e852b2eba8d493bec860d142ff54c2
Benjamin Cohen-Lhyver <cohenlhyver@protonmail.com>
2021-07-26 10:50:36 -0700

    mc

--- test_images_train.py
+++ test_images_train.py
@@ -2,11 +2,9 @@
 # @Author: Benjamin Cohen-Lhyver
 # @Date:   2021-07-24 10:15:59
 # @Last Modified by:   Benjamin Cohen-Lhyver
-# @Last Modified time: 2021-07-26 06:23:55
+# @Last Modified time: 2021-07-26 10:46:57
 
 import pickle
-
-# if __name__ == "__main__":
 
 with open("training_history.pkl", "rb") as history_file:
     history = pickle.load(history_file)
