7fb9c86c080f7b002cfb0cba7cc3726eaa9eb592
Benjamin Cohen-Lhyver <cohenlhyver@protonmail.com>
2021-07-26 06:27:12 -0700

    mbf

--- test_images_train.py
+++ test_images_train.py
@@ -2,7 +2,7 @@
 # @Author: Benjamin Cohen-Lhyver
 # @Date:   2021-07-24 10:15:59
 # @Last Modified by:   Benjamin Cohen-Lhyver
-# @Last Modified time: 2021-07-25 19:55:39
+# @Last Modified time: 2021-07-26 06:23:55
 
 import pickle
 
@@ -12,4 +12,4 @@ with open("training_history.pkl", "rb") as history_file:
     history = pickle.load(history_file)
 
 assert history["accuracy"][-1] > 0.95
-assert history["loss"] < 0.1
+assert history["loss"][-1] < 0.1
